19d976e129eaac42106e57836cd08b4e51a2fcd3
TJ Gbadebo <106924918+TGbadebo02@users.noreply.github.com>
2026-05-29 00:21:24 +0100

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int singleNum = 0;
+
+        for(int num : nums){
+            singleNum ^= num;
+        }
+
+        return singleNum;
+    }
+}
