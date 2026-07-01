edc19bf036b74bdf82582e4d2df588014e1c82e7
TJ Gbadebo <106924918+TGbadebo02@users.noreply.github.com>
2026-07-01 23:06:12 +0100

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.java
@@ -0,0 +1,33 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+
+        if(intervals.size() == 0) return true;
+        //sort the list.
+        Collections.sort(intervals, (a,b) -> Integer.compare(a.start,b.start));
+
+        Interval current = intervals.get(0);
+
+        for(int i = 1; i < intervals.size(); i++){
+            Interval next = intervals.get(i);
+
+            if(current.end <= next.start){
+                current = next;
+            }else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
