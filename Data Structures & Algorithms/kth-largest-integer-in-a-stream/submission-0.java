41a867bf9b6cfb6c9b0a860effcc02f91b0ef6e9
TJ Gbadebo <106924918+TGbadebo02@users.noreply.github.com>
2026-07-10 01:55:23 +0100

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.java
@@ -0,0 +1,27 @@
+class KthLargest {
+    private PriorityQueue<Integer> queue;
+    private int k;
+    
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.queue = new PriorityQueue<>();
+
+        for (int num : nums) {
+            queue.add(num);
+
+            if(queue.size() > k){
+                queue.poll();
+            }
+        }
+    }
+
+    public int add(int val) {
+        queue.add(val);
+
+        if(queue.size() > k){
+             queue.poll();
+        }
+
+        return queue.peek();
+    }
+}
