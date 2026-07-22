4b9d0a512c4eb1e69b04cdb8ee8ec5bece769628
TJ Gbadebo <106924918+TGbadebo02@users.noreply.github.com>
2026-07-22 23:24:07 +0100

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+    Set<Integer> set = new HashSet<>();
+
+    public boolean isHappy(int n) {
+
+        int newN = sumHelper(n);
+        
+        if(set.contains(newN)) return false;
+        
+        if (newN != 1) {
+            set.add(newN);
+            int nChk = sumHelper(newN);
+           return isHappy(nChk);
+        }
+
+        //if(set.contains(newN)) return false;
+
+        return true;
+    }
+
+    public int sumHelper(int n) {
+        int sum = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            int digitTwo = (int) Math.pow(digit, 2);
+            sum += digitTwo;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
