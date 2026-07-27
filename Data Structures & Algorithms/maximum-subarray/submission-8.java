a8f5b2650028f2bc4b1b18035b0a149c331af9fe
TJ Gbadebo <106924918+TGbadebo02@users.noreply.github.com>
2026-07-27 23:44:26 +0100

    Add: maximum-subarray - submission-8

---
+++ Data Structures & Algorithms/maximum-subarray/submission-8.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        //set base case.
+        if(nums.length == 0 || nums.length > 1000) return 0;
+
+        if(nums.length == 1) return nums[0];
+
+        int sum = nums[0];
+        int maxSum = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            if(sum < 0){
+                sum = 0;
+            }
+            sum += nums[i];
+            maxSum = Math.max(maxSum,sum);
+        }
+
+        return maxSum;
+    }
+}
