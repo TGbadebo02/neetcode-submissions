012f45af400bc1a08abbbd72306706150bff77e7
TJ Gbadebo <106924918+TGbadebo02@users.noreply.github.com>
2026-07-31 00:01:31 +0100

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums.length == 0) return false;
+
+        int n = nums.length;
+        int goal = n - 1;
+
+        for(int i = n - 2;i >= 0; i--){
+            if(goal - i <= nums[i]){
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+}
