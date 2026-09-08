554664118f8ce0c384fbc0d9a98517f6b91a3b0a
TJ Gbadebo <106924918+TGbadebo02@users.noreply.github.com>
2026-09-08 22:35:20 +0100

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+
+        int prefix = 1;
+        result[0] = 1;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            prefix *= nums[i];
+            result[i + 1] = prefix;
+        }
+
+        int suffix = 1;
+        for (int i = nums.length - 1; i > 0; i--) {
+            suffix *= nums[i];
+            result[i - 1] *= suffix;
+        }
+        return result;
+    }
+}
