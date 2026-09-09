9d1e68f7edd05682f7bb6356c0eaa33a0abb0aab
TJ Gbadebo <106924918+TGbadebo02@users.noreply.github.com>
2026-09-10 00:29:44 +0100

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/course-schedule/submission-4.java
@@ -0,0 +1,45 @@
+class Solution {
+    HashMap<Integer, List<Integer>> adjMap = new HashMap<>();
+    HashSet<Integer> set = new HashSet<>();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+       
+        for (int i = 0; i < numCourses; i++) {
+            adjMap.put(i, new ArrayList<>());
+        }
+
+        for (int[] pre : prerequisites) {
+            int course = pre[0];
+            int preqs = pre[1];
+
+            adjMap.get(course).add(preqs);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (dfs(i) == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean dfs(int course) {
+        if (set.contains(course))
+            return false;
+
+        if(adjMap.get(course).isEmpty()) {
+            return true;
+        }
+
+        set.add(course);
+
+        for (int pre : adjMap.get(course)) {
+            if (dfs(pre) == false) {
+                return false;
+            }
+        
+        }
+        set.remove(course);
+        adjMap.get(course).clear();
+        return true;
+    }
+}
